0ce40f50a5dbffe42b8cae5319ce1ea9d47d8300
ignaciohugog <ignaciohugog@gmail.com>
2024-10-03 08:57:31 +0200

    Initial Commit

---
+++ ClaraAssignment/ClaraAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaraAssignmentApp.swift
+//  ClaraAssignment
+//
+//  Created by Ignacio Gomez on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClaraAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaraAssignmentTests/ClaraAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaraAssignmentTests.swift
+//  ClaraAssignmentTests
+//
+//  Created by Ignacio Gomez on 03/10/2024.
+//
+
+import Testing
+@testable import ClaraAssignment
+
+struct ClaraAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
